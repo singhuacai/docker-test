666e10302e1b33a265ccb83effc67ca24cfc1f93
singhuacai <singhuacai@gmail.com>
2022-03-11 23:03:24 +0800

    test2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require("http-errors");
-var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var express = require("express");
+var createError = require("http-errors");
+var cookieParser = require("cookie-parser");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
